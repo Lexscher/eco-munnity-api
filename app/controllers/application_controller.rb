8b8fcbb14743b9fc831368042b9e9a6a8ab95fd8
Alexander Schelchere <ascellie@gmail.com>
2019-08-29 17:32:16 -0400

    controllers(app): use JWT to create token

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::API
+  # create a new token with the given payload
+  def encode_token(payload)
+    token = JWT.encode(payload, ENV["JWT_SECRET_KEY"], "HS256")
+  end
+
+  # Create the user payload
+  def user_payload(user)
+    { user_id: user.id }
+  end
 end
