c9fe66b23c96f2e86a410f0d9634597a008eab6b
Alexander Schelchere <ascellie@gmail.com>
2019-08-30 10:11:25 -0400

    application controller: methods to handle our token

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,30 @@
-class ApplicationController < ActionController::API
+coclass ApplicationController < ActionController::API
   # create a new token with the given payload
+  def secret
+    ENV["JWT_SECRET_KEY"]
+  end
+
   def encode_token(payload)
-    token = JWT.encode(payload, ENV["JWT_SECRET_KEY"], "HS256")
+    JWT.encode(payload, secret, "HS256")
+  end
+
+  # grab token from our headers
+  def token
+    request.headers["Authorization"]
+  end
+
+  # decode the token 
+  def decoded_token
+    JWT.decode(token, secret, { algorithm: "HS256"})
   end
 
   # Create the user payload
   def user_payload(user)
     { user_id: user.id }
   end
+
+  # Current user by way of decoded token's id
+  def current_user
+		User.find(decoded_token[0]["user_id"])
+	end
 end
