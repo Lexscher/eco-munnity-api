052273692bb79bdc5f9c1cb823e9df1f31d0e950
Alexander Schelchere <ascellie@gmail.com>
2019-08-30 10:13:45 -0400

    controller(users): add profile and show routes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,19 @@ class UsersController < ApplicationController
     end
   end
 
+  def profile
+    render json: current_user
+  end
+
+  def show
+    selected_user = User.find(params["id"])
+    if selected_user == current_user
+      render json: current_user
+    else
+      render json: selected_user
+    end
+  end
+
   private
 
   def user_params
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   post "/login", to: "auth#login"
   # create user AND create token
   post "/signup", to: "users#create"
+  # Get user profile
+  get  "/profile", to: "users#profile"
 
-  resources :users, only: [:show, :destroy]
+  resources :users, only: [:show]
 end
