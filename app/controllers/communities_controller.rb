aaead928a067aa0243de355ad11ecfa140b1d549
Alexander Schelchere <ascellie@gmail.com>
2019-08-30 12:31:03 -0400

    controllers(communities): add show and index

--- app/controllers/communities_controller.rb
+++ app/controllers/communities_controller.rb
@@ -1,2 +1,13 @@
 class CommunitiesController < ApplicationController
+
+  def index
+    communities = Community.all
+    render json: communities
+  end
+
+  def show
+   community = Community.find(params["id"])
+   render json: community
+  end
+
 end
