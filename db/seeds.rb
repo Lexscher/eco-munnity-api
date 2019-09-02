cd846c528e0970605e21701543a50df7ed6402ae
Alexander Schelchere <ascellie@gmail.com>
2019-09-02 13:27:24 -0400

    db(seed): update joined_communities, add voted_comments

--- db/seeds.rb
+++ db/seeds.rb
@@ -32,68 +32,68 @@ frank_post_on_sea = frank.posts.create(title: "We're doing more harm than good..
 
 # Comments
   # Comments for LEO's post under ../ANIMALS
-frank.comments.create(content: "Leo... PLEASE LET ME HAVE IT?!", post: leo_post_on_animals)
+comment1 = frank.comments.create(content: "Leo... PLEASE LET ME HAVE IT?!", post: leo_post_on_animals)
 
   # Comments for FRANK's post under ../SEA
-percy.comments.create(content: "Thank you, @frank! I've been trying to share this news for a while now.", post: frank_post_on_sea)
-frank.comments.create(content: "I know!! That's how I got so invested in doing research.", post: frank_post_on_sea)
-hazel.comments.create(content: "Hey! Do you two wanna organize an awareness walk?", post: frank_post_on_sea)
-frank.comments.create(content: "You bet!!", post: frank_post_on_sea)
-percy.comments.create(content: "That sounds Excellent, Hazel!", post: frank_post_on_sea)
+comment2 = percy.comments.create(content: "Thank you, @frank! I've been trying to share this news for a while now.", post: frank_post_on_sea)
+comment3 = frank.comments.create(content: "I know!! That's how I got so invested in doing research.", post: frank_post_on_sea)
+comment4 = hazel.comments.create(content: "Hey! Do you two wanna organize an awareness walk?", post: frank_post_on_sea)
+comment5 = frank.comments.create(content: "You bet!!", post: frank_post_on_sea)
+comment6 = percy.comments.create(content: "That sounds Excellent, Hazel!", post: frank_post_on_sea)
 
   # Comments for GHOSTBIRD's post under ../FORESTS
-ghostbird.comments.create(content: "...You guys???", post: ghostbird_post_on_forests)
-cynthia.comments.create(content: "I know, GB!! Try posting this in ../Disasters? I think that more people are in that community", post: ghostbird_post_on_forests)
-ghostbird.comments.create(content: "That's a good idea cynthia, I'll try that right now!", post: ghostbird_post_on_forests)
+comment7 = ghostbird.comments.create(content: "...You guys???", post: ghostbird_post_on_forests)
+comment8 = cynthia.comments.create(content: "I know, GB!! Try posting this in ../Disasters? I think that more people are in that community", post: ghostbird_post_on_forests)
+comment9 = ghostbird.comments.create(content: "That's a good idea cynthia, I'll try that right now!", post: ghostbird_post_on_forests)
 
   # Comments for GHOSTBIRD's post under ../DISASTERS
-frank.comments.create(content: "This is terrible!!", post: ghostbird_post_on_disasters)
-nico.comments.create(content: "Does anyone know where we can find a list of resources to help out?", post: ghostbird_post_on_disasters)
-piper.comments.create(content: "I'll brb, let me do some research", post: ghostbird_post_on_disasters)
-piper.comments.create(content: "Okay, I found this post - \"https://www.cnbc.com/2019/08/27/amazon-wildfires-how-to-help.html\", Please everyone let's work together!", post: ghostbird_post_on_disasters)
-cynthia.comments.create(content: "I'm with you, piper!", post: ghostbird_post_on_disasters)
-nico.comments.create(content: "Yeah, we can all help out!", post: ghostbird_post_on_disasters)
-frank.comments.create(content: "I'll start a fundraiser!", post: ghostbird_post_on_disasters)
-jason.comments.create(content: "Do you need any help, frank?", post: ghostbird_post_on_disasters)
-frank.comments.create(content: "Yeah, that'd be great!", post: ghostbird_post_on_disasters)
-hazel.comments.create(content: "@percy", post: ghostbird_post_on_disasters)
-percy.comments.create(content: "Woah thanks for tagging me, hazel!", post: ghostbird_post_on_disasters)
-ghostbird.comments.create(content: "Thank you everyone! This really means a lot!", post: ghostbird_post_on_disasters)
+comment10 = frank.comments.create(content: "This is terrible!!", post: ghostbird_post_on_disasters)
+comment11 = nico.comments.create(content: "Does anyone know where we can find a list of resources to help out?", post: ghostbird_post_on_disasters)
+comment12 = piper.comments.create(content: "I'll brb, let me do some research", post: ghostbird_post_on_disasters)
+comment13 = piper.comments.create(content: "Okay, I found this post - \"https://www.cnbc.com/2019/08/27/amazon-wildfires-how-to-help.html\", Please everyone let's work together!", post: ghostbird_post_on_disasters)
+comment14 = cynthia.comments.create(content: "I'm with you, piper!", post: ghostbird_post_on_disasters)
+comment15 = nico.comments.create(content: "Yeah, we can all help out!", post: ghostbird_post_on_disasters)
+comment16 = frank.comments.create(content: "I'll start a fundraiser!", post: ghostbird_post_on_disasters)
+comment17 = jason.comments.create(content: "Do you need any help, frank?", post: ghostbird_post_on_disasters)
+comment18 = frank.comments.create(content: "Yeah, that'd be great!", post: ghostbird_post_on_disasters)
+comment19 = hazel.comments.create(content: "@percy", post: ghostbird_post_on_disasters)
+comment20 = percy.comments.create(content: "Woah thanks for tagging me, hazel!", post: ghostbird_post_on_disasters)
+comment21 = ghostbird.comments.create(content: "Thank you everyone! This really means a lot!", post: ghostbird_post_on_disasters)
 
   # Comments for CONTROL's post under ../ANOMALIES
-cynthia.comments.create(content: "Yep! It definitely has something to do with what's behind the border.", post: control_post_on_anomalies)
-control.comments.create(content: "Wait, what? How are you so sure?", post: control_post_on_anomalies)
-cynthia.comments.create(content: "It's kind of my fault...", post: control_post_on_anomalies)
-control.comments.create(content: "Stop wasting my time! You don't even know who I'm talking about, or what the border is.", post: control_post_on_anomalies)
-cynthia.comments.create(content: "You're talking about Whitby. And John, you should really do more research when you start a new job. If you had, you'd know that I preceded you.", post: control_post_on_anomalies)
-control.comments.create(content: "Wait, you--", post: control_post_on_anomalies)
-control.comments.create(content: "Tell me everything!", post: control_post_on_anomalies)
-control.comments.create(content: "Cynthia??", post: control_post_on_anomalies)
-annabeth.comments.create(content: "If she doesn't respond, reach out to me. Maybe we can help eachother...", post: control_post_on_anomalies)
+comment22 = cynthia.comments.create(content: "Yep! It definitely has something to do with what's behind the border.", post: control_post_on_anomalies)
+comment23 = control.comments.create(content: "Wait, what? How are you so sure?", post: control_post_on_anomalies)
+comment24 = cynthia.comments.create(content: "It's kind of my fault...", post: control_post_on_anomalies)
+comment25 = control.comments.create(content: "Stop wasting my time! You don't even know who I'm talking about, or what the border is.", post: control_post_on_anomalies)
+comment26 = cynthia.comments.create(content: "You're talking about Whitby. And John, you should really do more research when you start a new job. If you had, you'd know that I preceded you.", post: control_post_on_anomalies)
+comment27 = control.comments.create(content: "Wait, you--", post: control_post_on_anomalies)
+comment28 = control.comments.create(content: "Tell me everything!", post: control_post_on_anomalies)
+comment29 = control.comments.create(content: "Cynthia??", post: control_post_on_anomalies)
+comment30 = annabeth.comments.create(content: "If she doesn't respond, reach out to me. Maybe we can help eachother...", post: control_post_on_anomalies)
 
 # ------------------
 
 # Joined Communities
   # users join ../Anomalies
-control.join_community(anomalies)
-cynthia.join_community(anomalies)
-annabeth.join_community(anomalies)
+control.joined_communities.create(community: anomalies)
+cynthia.joined_communities.create(community: anomalies)
+annabeth.joined_communities.create(community: anomalies)
 
- # users join ../Disasters
- frank.join_community(disasters)
- nico.join_community(disasters)
- piper.join_community(disasters)
- ghostbird.join_community(disasters)
- hazel.join_community(disasters)
+ # users joined ../Disasters
+ frank.joined_communities.create(community: disasters)
+ nico.joined_communities.create(community: disasters)
+ piper.joined_communities.create(community: disasters)
+ ghostbird.joined_communities.create(community: disasters)
+ hazel.joined_communities.create(community: disasters)
  
  # ../Forests
- cynthia.join_community(forests)
+ cynthia.joined_communities.create(community: forests)
 
 #  ../Animals
- leo.join_community(animals)
+ leo.joined_communities.create(community: animals)
 
 #  ../Sea
-frank.join_community(sea)
+frank.joined_communities.create(community: sea)
 
 # Voted Posts
 
@@ -113,9 +113,44 @@ annabeth.voted_posts.create(state: true, post: control_post_on_anomalies)
 percy.voted_posts.create(state: true, post: frank_post_on_sea)
 hazel.voted_posts.create(state: true, post: frank_post_on_sea)
 
-# voted Comments
+# Voted Comments
+
+  # Cynthia's comment for GHOSTBIRD's post under ../FORESTS
+ghostbird.voted_comments.create(state: true, comment: comment8)
+
+  # Like Piper's comment in GHOSTBIRD's post under ../DISASTERS
+hazel.voted_comments.create(state: true, comment: comment13)
+frank.voted_comments.create(state: true, comment: comment13)
+ghostbird.voted_comments.create(state: true, comment: comment13)
+cynthia.voted_comments.create(state: true, comment: comment13)
+nico.voted_comments.create(state: true, comment: comment13)
+percy.voted_comments.create(state: true, comment: comment13)
+saul.voted_comments.create(state: true, comment: comment13)
+jason.voted_comments.create(state: true, comment: comment13)
+
+# Ghost Bird's comment for GHOSTBIRD's post under ../DISASTERS
+hazel.voted_comments.create(state: true, comment: comment21)
+cynthia.voted_comments.create(state: true, comment: comment21)
+jason.voted_comments.create(state: true, comment: comment21)
+piper.voted_comments.create(state: true, comment: comment21)
+percy.voted_comments.create(state: true, comment: comment21)
+frank.voted_comments.create(state: true, comment: comment21)
+nico.voted_comments.create(state: true, comment: comment21)
+
+  # Cynthia's comment for CONTROL's post under ../ANOMALIES
+annabeth.voted_comments.create(state: true, comment: comment26)
+ghostbird.voted_comments.create(state: true, comment: comment26)
+saul.voted_comments.create(state: true, comment: comment26)
+jason.voted_comments.create(state: true, comment: comment26)
+percy.voted_comments.create(state: true, comment: comment26)
 
 
+# Hazel's comment for FRANK's post under ../SEA
+percy.voted_comments.create(state: true, comment: comment4)
+frank.voted_comments.create(state: true, comment: comment4)
+
+
+# ----
 # Moderators
 
 
