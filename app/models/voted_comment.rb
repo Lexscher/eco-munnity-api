e929e7b9d8a9960600ee7a5296f5d76bd6aad042
Alexander Schelchere <ascellie@gmail.com>
2019-09-02 13:08:35 -0400

    models(VotedComment): add validations

--- app/models/voted_comment.rb
+++ app/models/voted_comment.rb
@@ -1,4 +1,19 @@
 class VotedComment < ApplicationRecord
+  # Relationships
   belongs_to :user
   belongs_to :comment
+
+  # Validations
+  validates :user_id, :comment_id, presence: true
+  validates :user_id, uniqueness: {
+    scope: :comment_id,
+    message: "You can only vote on a comment once!!"
+  }
+
+  # Custom Validations
+  validate :state_is_not_nil
+
+  def state_is_not_nil
+    self.errors.add(:state, "State can't be nil.") if state == nil 
+  end
 end
