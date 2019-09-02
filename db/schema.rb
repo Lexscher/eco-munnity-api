5d769fa016f21c7e8ee11ffe665401967abd88ad
Alexander Schelchere <ascellie@gmail.com>
2019-09-01 23:17:24 -0400

    db(schema): run joined_community migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_01_185622) do
+ActiveRecord::Schema.define(version: 2019_09_02_030110) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,6 +32,15 @@ ActiveRecord::Schema.define(version: 2019_09_01_185622) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_communities_on_user_id"
+  end
+
+  create_table "joined_communities", force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "community_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["community_id"], name: "index_joined_communities_on_community_id"
+    t.index ["user_id"], name: "index_joined_communities_on_user_id"
   end
 
   create_table "posts", force: :cascade do |t|
@@ -58,6 +67,8 @@ ActiveRecord::Schema.define(version: 2019_09_01_185622) do
   add_foreign_key "comments", "posts"
   add_foreign_key "comments", "users"
   add_foreign_key "communities", "users"
+  add_foreign_key "joined_communities", "communities"
+  add_foreign_key "joined_communities", "users"
   add_foreign_key "posts", "communities"
   add_foreign_key "posts", "users"
 end
