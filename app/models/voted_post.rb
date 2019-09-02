525ec46a09a9120b2e0ef3a2be7a412cc930fc47
Alexander Schelchere <ascellie@gmail.com>
2019-09-02 09:43:11 -0400

    VotedPosts: add custom validations

--- app/models/voted_post.rb
+++ app/models/voted_post.rb
@@ -2,11 +2,19 @@ class VotedPost < ApplicationRecord
   # Relationships
   belongs_to :user
   belongs_to :post
-
+  
   # Validations
-  validates :state, :user_id, :post_id, presence: true
+  validates :user_id, :post_id, presence: true
   validates :user_id, uniqueness: {
     scope: :post_id,
     message: "You can only vote on a post once!!"
   }
+
+  # Custom Validations
+  validate :state_is_not_nil
+
+  def state_is_not_nil
+    self.errors.add(:state, "State can't be nil.") if state == nil 
+  end
+  
 end
